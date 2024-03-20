1bef28ebff592b040bd739e3370196e01e1f6cad
User <samosyk.evgen.92@mail.ru>
2024-03-20 21:40:50 +0300

    web application2222222

--- src/main/java/org/example/HelloServlet.java
+++ src/main/java/org/example/HelloServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+@WebServlet
 public class HelloServlet extends HttpServlet {
 
     @Override
